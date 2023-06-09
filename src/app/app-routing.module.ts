5774599a565adcd85783d7828f73d87d430cd8b4
CarlosSSC <a6520150015@utch.edu.mx>
2023-06-08 19:58:27 -0600

    Frontend 1st deliverable
    all of the frontend of the first deliverable is finished

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,20 @@ const routes: Routes = [
   {
     path: 'signup',
     loadChildren: () => import('./pages/signup/signup.module').then( m => m.SignupPageModule)
+  },  {
+    path: 'order-selection',
+    loadChildren: () => import('./pages/order-selection/order-selection.module').then( m => m.OrderSelectionPageModule)
   },
+  {
+    path: 'main-screen',
+    loadChildren: () => import('./pages/main-screen/main-screen.module').then( m => m.MainScreenPageModule)
+  },
+  {
+    path: 'item-information',
+    loadChildren: () => import('./pages/item-information/item-information.module').then( m => m.ItemInformationPageModule)
+  },
+
+
 ];
 
 @NgModule({
---
+++ src/app/pages/order-selection/order-selection.page.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-order-selection',
+  templateUrl: './order-selection.page.html',
+  styleUrls: ['./order-selection.page.scss'],
+})
+export class OrderSelectionPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
+
+
